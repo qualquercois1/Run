ed96b2ff017dca941708738b99ad43e45ccc22ac
Cassio dos Santos Rodrigues <0030CE27269@buriti.df.gov.br>
2026-01-08 11:49:38 -0300

    feat: obstaculos e movimentação

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ gray = (70,70,70)
 dark_gray = (54,54,54)
 red = (255,0,0)
 black = (0,0,0)
+blue = (0,0,200)
 
 # Variaveis
 # tela
@@ -18,6 +19,7 @@ fps = 30
 running = True
 track_division = 3 # quantas pistas terão
 gap = 5
+num_obstacles = 2
 
 # setup da janela
 window = pygame.display.set_mode((win_width, win_height))
@@ -51,10 +53,31 @@ class Car:
         rect = pygame.Rect(self.position*div_pos+(div_pos-self.size_x)//2, game_height-self.size_y-gap, self.size_x, self.size_y)
         pygame.draw.rect(surface, self.color, rect, border_radius=4)
 
+class Obstacle:
+    def __init__(self, position):
+        self.y = 0
+        self.size_x = div_pos-150
+        self.size_y = 100
+        self.color = blue
+        self.velocity = 3
+        self.position = position
+
+    def draw(self, surface):
+        rect = pygame.Rect(self.position*div_pos+(div_pos-self.size_x)//2, self.y, self.size_x, self.size_y)
+        pygame.draw.rect(surface, self.color, rect, border_radius=4)
+
+    def move(self):
+        self.y += self.velocity
+
+
 
 def run_game():
     global running
     car = Car()
+    obstacles = []
+    for i in range(0, num_obstacles):
+        obstacle = Obstacle(random.randint(0,track_division-1))
+        obstacles.append(obstacle)
 
     while running:
         for event in pygame.event.get():
@@ -68,6 +91,10 @@ def run_game():
 
         game_screen.blit(background, (0,0)) # desenha o background
         
+        for y in obstacles:
+            y.draw(game_screen)
+            y.move()
+
         car.draw(game_screen)
 
         window.fill(black)
