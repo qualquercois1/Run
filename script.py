a9a052a9f5f1b3d5a0e21488eaccfef780613a67
Cassio dos Santos Rodrigues <0030CE27269@buriti.df.gov.br>
2026-01-08 11:27:03 -0300

    feat: movimentação do carro

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ init_y = (win_height-game_height) // 2
 fps = 30
 running = True
 track_division = 3 # quantas pistas terão
+gap = 5
 
 # setup da janela
 window = pygame.display.set_mode((win_width, win_height))
@@ -24,34 +25,55 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Run")
 
 # background
-game_screen = pygame.Surface((game_width, game_height)).convert()
-game_screen.fill(dark_gray)
+background = pygame.Surface((game_width, game_height)).convert()
+background.fill(dark_gray)
 div_pos = game_width//track_division
 for x in range(1, track_division):
     x_real = x * div_pos
-    pygame.draw.line(game_screen, black, (x_real,0), (x_real, game_height))
-game_screen = game_screen.convert()
+    pygame.draw.line(background, black, (x_real,0), (x_real, game_height))
+game_screen = pygame.Surface((game_width, game_height)).convert()
 
 
 class Car:
     def __init__(self):
-        self.position = 1 # 0,1,2...
+        self.position = track_division // 2
+        self.size_x = div_pos-250
+        self.size_y = 100
+        self.color = red
+
+    def move(self, direction):
+        if direction == "RIGHT" and self.position != track_division-1:
+            self.position += 1
+        elif direction == "LEFT" and self.position:
+            self.position -= 1
+
+    def draw(self, surface):
+        rect = pygame.Rect(self.position*div_pos+(div_pos-self.size_x)//2, game_height-self.size_y-gap, self.size_x, self.size_y)
+        pygame.draw.rect(surface, self.color, rect, border_radius=4)
 
 
 def run_game():
     global running
+    car = Car()
+
     while running:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d: 
+                    car.move("RIGHT")
+                if event.key == pygame.K_a: 
+                    car.move("LEFT")
+
+        game_screen.blit(background, (0,0)) # desenha o background
+        
+        car.draw(game_screen)
 
         window.fill(black)
-        
-        clock.tick(fps)
         window.blit(game_screen, (init_x, init_y))
         pygame.display.flip()
+        clock.tick(fps)
 
     pygame.quit()
     sys.exit()
